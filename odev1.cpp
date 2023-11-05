1e3916daa613159d0cfd122c4df786222e2fcbad
Deneyap-Emir-adam <149937130+Deneyap-Emir-adam@users.noreply.github.com>
2023-11-05 18:41:56 +0300

    Deneyap Ödevler

---
+++ odev1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const float pi = 3.14;
+    int r = 18;
+
+    cout << "Dairenin Cevresi:" << (r*2)*pi << endl;
+    cout << "Dairenin Alani:" << (r*r)*pi << endl;
+
+    return (0);
+}
